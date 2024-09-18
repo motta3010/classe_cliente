665ef4d43aa82cefc0b2fc96f2f9325777c06208
motta3010 <163685698+motta3010@users.noreply.github.com>
2024-09-18 19:59:34 -0300

    att

---
+++ classe_cliente.java
@@ -0,0 +1,24 @@
+package Sistema_cliente;
+
+public class Cliente {
+    private int id_cliente;
+    private String Nome_cli;
+    private Tipo_Documento old_Tipo_Documento;
+    private int Cod_Docum;
+    private int Rg_Cli;
+    private String Endereço;
+    private String Bairro;
+    private String Cidade;
+    private String Estado;
+    private String CEP;
+    private int DDD_res;
+    private String Tel_res;
+    private int DDD_com;
+    private String Tel_com;
+    private int DDD_cel;
+    private String Tel_cel;
+    private int E_mail;
+    private int Status_cli;
+    private Date Data_cad_Cliente;
+    
+}
